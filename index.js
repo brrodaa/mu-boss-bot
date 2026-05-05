cca0c6c23e6485adc53a4ab08d4f142477772450
brrodaa <72532487+brrodaa@users.noreply.github.com>
2026-05-05 13:39:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const {
 } = require("discord.js");
  
 const fs = require("fs");
-const config = require("./config.json");
+const TOKEN = process.env.TOKEN;
+const CHANNEL_ID = process.env.CHANNEL_ID;
  
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
